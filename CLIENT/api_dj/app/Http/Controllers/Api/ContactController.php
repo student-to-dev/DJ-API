6ab9de2a7f4ba3994b1b17b9c678580dc9464896
SergejsRom <sergejsromanovskis@gmail.com>
2022-10-05 00:26:10 +0300

    Installed lara, controller, resource, migration, model, get route to test is it working

---
+++ CLIENT/api_dj/app/Http/Controllers/Api/ContactController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Contact;
+use App\Http\Resources\ContactResource;
+
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        $contacts = Contact::select('email', 'name', 'surname', 'phone')->get();
+        return ContactResource::collection($contacts);
+    }
+}
