e1deccbc82bdd56160b753889d9e99b72eb28005
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-10-11 11:50:22 +0300

    response fix

--- project/app/Http/Controllers/NameController.php
+++ project/app/Http/Controllers/NameController.php
@@ -6,14 +6,14 @@ use App\Models\Name;
 use Illuminate\Http\Request;
 use App\Http\Controllers\StatsController;
 
-
 class NameController extends Controller
 {
-    public function store(Request $request)
+    public function store(StatsController $stats, Request $request)
     {
         $data = $request->input('name');
         $response = Name::class()->validate($data);
+        $stats->store($data);
         return response($response, $response[1]);
-        new StatsController::class, 'store';
+        
     }
 }
