989a98b7039e45030c67b29fce74426e47d01342
SergejsRom <sergejsromanovskis@gmail.com>
2022-10-18 14:27:47 +0300

    counts errors in showNameStats

--- project/app/Http/Controllers/StatsController.php
+++ project/app/Http/Controllers/StatsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Stats;
+use App\Models\Name;
+use Illuminate\Http\Request;
 
 class StatsController extends Controller
 {
@@ -13,11 +15,17 @@ class StatsController extends Controller
 
     public function showStats(Stats $stats)
     {
+        
     }
 
     public function showNameStats()
     {
-        
+       // function totalRepeatedName() {
+            $total = Name::groupBy('errors')
+            ->selectRaw('count(*) as count, errors')
+            ->pluck('count', 'errors');
+            return $total->all();
+      //  }
     }
 
     public function showEmailStats()
