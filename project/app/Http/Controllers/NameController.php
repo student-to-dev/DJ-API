64bbf4b1b7a1bdbe564870115fccc8e4fedb072a
SergejsRom <sergejsromanovskis@gmail.com>
2022-10-20 19:39:54 +0300

    seems OK regex for name field

--- project/app/Http/Controllers/NameController.php
+++ project/app/Http/Controllers/NameController.php
@@ -20,14 +20,38 @@ class NameController extends Controller
            'nameSurname' => ['required', 'min:3', 'max:35',
                            // 'not_regex:/^[0-9]+$/',
                             //'regex:/^[a-zA-Z\'\-\040]+$/',
-                            'regex:/~^(?:[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+\s[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+\s?)+$/u', 
+                           // 'regex:/^[\\p{L} \'.-]+$/',
+                          'regex:/^(?:[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+\s[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+\s?)+$/u', 
+
+                                                /* ^   # start of subject
+                            (?:     # match this:
+                                [           # match a:
+                                    \p{L}       # Unicode letter, or
+                                    \p{Mn}      # Unicode accents, or
+                                    \p{Pd}      # Unicode hyphens, or
+                                    \'          # single quote, or
+                                    \x{2019}    # single quote (alternative)
+                                ]+              # one or more times
+                                \s          # any kind of space
+                                [               #match a:
+                                    \p{L}       # Unicode letter, or
+                                    \p{Mn}      # Unicode accents, or
+                                    \p{Pd}      # Unicode hyphens, or
+                                    \'          # single quote, or
+                                    \x{2019}    # single quote (alternative)
+                                ]+              # one or more times
+                                \s?         # any kind of space (0 or more times)
+                            )+      # one or more times
+                        $   # end of subject
+                        u #to read unicode */
+
                         ],      
        ],
                 [
-                'nameSurname.min' => 'Too low characters',      // nameTooShort  surnameTooShort
+                'nameSurname.min' => 'Field must be at least 3 characters',      // nameTooShort  surnameTooShort
                 'nameSurname.required' => 'Empty field',
-                'nameSurname.max' => 'Too much',                // nameTooLong
-                'nameSurname.regex' => 'wrong character(s)',    // nameWrongCharacter
+                'nameSurname.max' => 'Too much symbols, max allowed 35 characters',                // nameTooLong
+                'nameSurname.regex' => 'field must be in Name Surname format, numbers and special characters are not allowed',    // nameWrongCharacter
                // 'nameSurname.not_regex' => 'no numbers',    // nameWrongCharacter
                 ]
        );
