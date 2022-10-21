c0a70fb52eb7124fe013970e8fd1ec53fa0d0b46
SergejsRom <sergejsromanovskis@gmail.com>
2022-10-21 22:21:59 +0300

    show stats

--- project/app/Http/Controllers/StatsController.php
+++ project/app/Http/Controllers/StatsController.php
@@ -16,8 +16,22 @@ class StatsController extends Controller
 
     public function showStats(Stats $stats)
     {
-        $allerrors = collect($this->showNameStats())->merge($this->showEmailStats());
-        return $allerrors;
+        $statsall = DB::table('stats')
+        ->select(DB::Raw('nameEmpty as "Empty field"'), 
+        DB::Raw('nameTooShort as "Field must be at least 3 characters"'),
+        DB::Raw('nameWrongCharacter as "field must be in Name Surname format, numbers and special characters are not allowed"'),
+        DB::Raw('nameTooLong as "Too much symbols, max allowed 35 characters"'))
+        ->get();
+      // dd($statsall);
+        return $statsall;
+
+        // $stats = Stats::all();
+        // return $stats;
+
+        // $allerrors = collect($this->showNameStats())->merge($this->showEmailStats());
+        // return $allerrors;
+
+
     }
 
     public function showNameStats()
